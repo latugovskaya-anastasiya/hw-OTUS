11a40129243f17e18c23380e3754748898fff0d2
latugovskaya-anastasiya <latugovskayawork@gmail.com>
2022-06-28 05:00:42 +1000

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -11,5 +11,69 @@ type Task func() error
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	// Place your code here.
+	resChan := make(chan error)
+	jobChan := make(chan Task, len(tasks))
+
+	for i := 0; i < len(tasks); i++ {
+		jobChan <- tasks[i]
+	}
+
+	failures := 0
+	isRunning := true
+	freeWorkers := n
+
+	var graceful bool
+	var waitList int
+	var errOverload error
+	var finished int
+
+	for isRunning {
+		select {
+		case job, ok := <-jobChan:
+			if ok && freeWorkers > 0 && !graceful {
+				go func() {
+					resChan <- job()
+				}()
+				freeWorkers--
+				waitList++
+			}
+			if !ok && waitList <= 0 {
+				graceful = true
+				isRunning = false
+				close(jobChan)
+			}
+
+		case e, ok := <-resChan:
+			if ok && freeWorkers < n {
+				freeWorkers++
+				waitList--
+			}
+			if e != nil {
+				failures++
+			}
+			if failures >= m {
+				graceful = true
+			}
+			if failures >= m && waitList <= 0 {
+				errOverload = ErrErrorsLimitExceeded
+				graceful = true
+				isRunning = false
+				close(resChan)
+				close(jobChan)
+			}
+			finished++
+			if waitList <= 0 && finished == len(tasks) {
+				graceful = true
+				isRunning = false
+				close(resChan)
+				close(jobChan)
+			}
+		}
+	}
+
+	if errOverload != nil {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
