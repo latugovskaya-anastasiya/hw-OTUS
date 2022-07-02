7f3488ac0f7a38350d8caa6851ac968cb8cb3f9a
latugovskaya-anastasiya <latugovskayawork@gmail.com>
2022-07-02 12:03:05 +1000

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -5,10 +5,36 @@ type (
 	Out = In
 	Bi  = chan interface{}
 )
-
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = runner(done, stage(out))
+	}
+	return out
+}
+
+func runner(done, in In) Out {
+	innerOut := make(Bi)
+	go func() {
+		defer close(innerOut) // close when there is nothing to write
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in: // checking if we can read in the channel
+				if !ok {
+					return
+				}
+				// implementation of the ability to stop pipeline
+				select {
+				case <-done:
+					return
+				case innerOut <- v:
+				}
+			}
+		}
+	}()
+	return innerOut
 }
